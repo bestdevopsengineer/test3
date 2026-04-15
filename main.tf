6b71d138aa7fac8a80dec563241c697625b60549
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-04-15 08:46:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
-    terraform {
-       backend "remote" {
-         organization = "example-organization"
-       }
-     }
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "env/prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
