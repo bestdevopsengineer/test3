08fcdc7b2dc4e8df5c64c14f0b55fcdeacbcec28
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-04-15 08:47:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "my-terraform-state-bucket"
     key            = "env/prod/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+
   }
 }
